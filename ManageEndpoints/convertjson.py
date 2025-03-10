c6797638eafb336a05f99b86eec29258641b4aea
Kreindo <danielfdelgado.dg@gmail.com>
2025-03-09 19:32:57 -0600

    fix: typo error in module "convertjson"

--- ManageEndpoints/convertjson.py
+++ ManageEndpoints/convertjson.py
@@ -4,11 +4,11 @@ import json
 
 def getendpoint(endpoint, payload, filename):
     obtainedEndPoint = requests.get(endpoint, params=payload)
-    jsonEndPoint = obtainedEndPoint.json
+    jsonEndPoint = obtainedEndPoint.json()
 
     with open(filename, "w") as file:
         json.dump(jsonEndPoint, file, indent=4)
 
     with open(filename, "r") as file:
         jsonEndPoint = json.load(file)
-    pass
+    
